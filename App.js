727faab14d4853e6246d345d17363e8f0f2b72fc
Kim Jongwon <kimjongwon@Kimui-MacBook-Pro.local>
2019-01-07 22:24:44 +0900

    commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Dimensions, Platform , TextInput} from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Statusbar barStyle = "light-content" />
+        <StatusBar 
+          barStyle = "light-content"
+        />
+        <Text style={styles.title}  >Kawai To do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"}></TextInput>
+        </View>
       </View>
     );
   }
@@ -15,7 +23,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F23657',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
+  title : {
+    color : "white" ,
+    fontSize : 30,
+    marginTop : 50,
+    fontWeight : '200',
+    marginBottom : 30
+  },
+  card : {
+    backgroundColor : "white",
+    flex : 1,
+    width: width - 25,
+    borderTopLeftRadius : 10,
+    borderTopRightRadius : 10,
+    ...Platform.select({
+      ios:{
+        shadowColor : "rgb(50,50,50)",
+        shadowOpacity : 0.5,
+        shadowRadius : 5,
+        shadowOffset:{
+          height : -1,
+          width : 0
+        }
+      },
+      android:{
+        elevation : 3
+      }
+    })
+  },
+  input : {
+    fontSize : 10
+  }
 });
