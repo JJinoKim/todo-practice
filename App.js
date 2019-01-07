bfa1f498a7da8ae4c9a38705ea9abea915de9338
Kim Jongwon <kimjongwon@Kimui-MacBook-Pro.local>
2019-01-07 21:40:16 +0900

    commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>ㅂㅏ보성은</Text>
+        <Statusbar barStyle = "light-content" />
       </View>
     );
   }
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
     justifyContent: 'center',
   },
