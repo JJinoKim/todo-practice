7bb0ead4e7bace0a18950e6674219a5221acbcbb
Kim Jongwon <kimjongwon@Kimui-MacBook-Pro.local>
2019-01-08 23:07:52 +0900

    커밋

--- ToDo.js
+++ ToDo.js
@@ -1,15 +1,87 @@
 import React,{ Component } from "react";
-import { View, Text, StyleSheet} from "react-native"
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity} from "react-native"
+
+const { width, height } = Dimensions.get("window")
 
 export default class ToDo extends Component{
+    state = {
+        isEditing : false,
+        isComplete : false
+    };
     render(){
+        const {isComplete} = this.state;
         return(
-            <View>
-                <Text>Hello I'm a To Do</Text>
+            <View style={styles.container}>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View style={[styles.circle, isComplete ? styles.completedCircle : styles.uncompletedCircle]} />
+                    </TouchableOpacity>
+                    <Text style={[styles.text, isComplete ? styles.completedText : styles.uncompletedText ]}>Hello I'm a To Do</Text>
+                </View>
+                <View style={styles.column}>
+                    {isEditing ? (
+                        <View style={styles.actions}>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✅</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actionText}>✅</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
             </View>
         );
-        
+    }
+
+    _toggleComplete = () =>{
+        this.setState(prevState =>{
+            return({
+                isComplete : !prevState.isComplete
+            })
+        })
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container : {
+        width : width -50,
+        borderBottomColor : "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection : "row",
+        alignItems : "center"
+    },
+    circle : {
+        width : 30,
+        height : 30,
+        borderRadius : 15,
+        borderColor : "red",
+        borderWidth : 3,
+        marginRight : 20
+    },
+    text : {
+        fontWeight : "600",
+        fontSize : 20,
+        marginVertical : 20
+    },
+    completedCircle :{
+        borderColor  : "#bbb"
+    },
+    uncompletedCircle : {
+        borderColor : "#F23657"
+    },
+    completedText : {
+        color : "#bbb",
+        textDecorationLine : "line-through"
+    },
+    uncompletedText : {
+        color : "#353839"
+    }
+});
